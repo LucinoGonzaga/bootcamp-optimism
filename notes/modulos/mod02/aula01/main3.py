e961dc5583ed45538f65cf242f3a6db1e8a087ae
Lucino Gonzaga <lucino2@gmail.com>
2024-03-05 18:51:06 -0300

    Melhorias na aula 01

--- notes/modulos/mod02/aula01/main3.py
+++ notes/modulos/mod02/aula01/main3.py
@@ -5,5 +5,5 @@ seed = "jump skull butter topic bronze member feed wait flee oven deer rabbit"
 Account.enable_unaudited_hdwallet_features()
 account = Account().from_mnemonic(mnemonic=seed)
 
-print(account.key.hex())
-print(account.address)
+print('Chave privada: ',  account.key.hex())
+print('Chave publica: ', account.address)
